03a1932c8d91a530472c213add8cd62378ef381a
Z1aaan <zr.gntn@gmail.com>
2023-12-20 02:22:54 +0800

    fixed script for color changing

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+// Code for obin-odayo.github.io
+// Created by Zhean Robby L. Ganituen (Obin Odayo)
+// Created on December 20, 2023
+
+// Code for logo
 const logos = document.querySelectorAll('.logo');
 const links = document.querySelectorAll('.link-remove');
 
+let isHoveringLink = false; // Flag to check if any link is being hovered
+
 links.forEach(link => {
   link.addEventListener('mouseenter', function() {
+    isHoveringLink = true; 
     const altSrc = this.getAttribute('data-alt-src');
     logos.forEach(logo => {
       logo.setAttribute('src', altSrc);
@@ -10,16 +18,39 @@ links.forEach(link => {
   });
 
   link.addEventListener('mouseleave', function() {
-    logos.forEach(logo => {
-      const originalSrc = logo.getAttribute('src');
-      const altSrc = logo.getAttribute('data-alt-src');
-      if (altSrc && logo.getAttribute('src') !== originalSrc) {
-        logo.setAttribute('src', originalSrc);
-      }
-    });
+    isHoveringLink = false; 
+    resetToRed(); 
   });
 });
 
+function resetToRed() {
+  if (!isHoveringLink) {
+    logos.forEach(logo => {
+      const originalSrc = logo.getAttribute('src');
+      const redSrc = '/icon/iconRed.svg';
+      if (originalSrc !== redSrc) {
+        logo.setAttribute('src', redSrc);
+      }
+    });
+  }
+}
+
+logos.forEach(logo => {
+  logo.addEventListener('mouseenter', function() {
+    const altSrc = this.getAttribute('data-alt-src');
+    if (!isHoveringLink && altSrc !== '/icon/iconOrange.svg') {
+      this.setAttribute('src', altSrc);
+    }
+  });
+
+  logo.addEventListener('mouseleave', function() {
+    if (!isHoveringLink) {
+      resetToRed();
+    }
+  });
+});
+
+// Code for clock
 function updateTime() {
   const currentTimeParagraph = document.getElementById('currentTime');
   const now = new Date();
@@ -29,8 +60,6 @@ function updateTime() {
   currentTimeParagraph.textContent = `${hours}:${minutes}:${seconds}`;
 }
 
-// Update the time every second
 setInterval(updateTime, 1000);
 
-// Initial call to set the time immediately when the page loads
 updateTime();
